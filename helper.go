a6c72a46d716be189b545c9b612eeecae2d8046a
Pablo <pablogvivo@gmail.com>
2024-06-16 15:03:57 +0200

    add executable path

--- helper.go
+++ helper.go
@@ -1,9 +1,26 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 const configPath = "./config/config-%s"
 
 func GetPathEnv(environment string) string {
+	getExecutablePath()
 	return fmt.Sprintf(configPath, environment)
+}
+func getExecutablePath() string {
+	// Get the absolute path of the executable binary
+	exePath, err := os.Executable()
+	if err != nil {
+		panic(err) // Handle error gracefully
+	}
+
+	// Get the directory of the executable binary
+	exeDir := filepath.Dir(exePath)
+
+	return exeDir
 }
